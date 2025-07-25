b63442d7ec9937f3c357c5dfc7e603d4c1191110
dustinbroussard <dustin.broussard@gmail.com>
2025-07-25 18:42:00 -0500

    speach to text

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { VitePWA } from "vite-plugin-pwa";
 import { readFileSync } from "fs";
 
@@ -19,11 +18,9 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-    VitePWA({
+    mode === 'development' &&    VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'icons/*', 'lovable-uploads/*'],
+      includeAssets: ['robots.txt', 'icons/*', 'uploads/*'],
       manifest,
       workbox: {
         navigateFallback: 'offline.html'
